3fc61d4bd63c25e8edc5d9451d00fb6fb2d04925
Francis Otieno <info.francisotieno@gmail.com>
2026-01-18 07:39:34 +0300

    Delete config directory

--- config/supabase.js
+++
@@ -1,9 +0,0 @@
-        const SUPABASE_CONFIG = {
-            url: 'https://vryzwufhpooxgfholuqk.supabase.co',
-            anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZyeXp3dWZocG9veGdmaG9sdXFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg2MzQxMjYsImV4cCI6MjA4NDIxMDEyNn0.QT9ZI-ckTK3oMgzPvJlqFKzZFNFNkvbVbGTncxop__A'
-        };
-
-        const supabase = window.supabase.createClient(
-            SUPABASE_CONFIG.url,
-            SUPABASE_CONFIG.anonKey
-        );
